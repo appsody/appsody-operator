13d7db02961250c3e06a961cd8c7f61456ad0294
Ethan Johnston <ethan.johnston@ibm.com>
2019-07-23 13:26:50 -0400

    Extended basic e2e test skeleton

--- test/e2e/appsody_test.go
+++ test/e2e/appsody_test.go
@@ -2,29 +2,55 @@ package e2e
 
 import (
 	"testing"
+	"time"
 
+	"github.com/appsody-operator/pkg/apis"
+	appsody "github.com/appsody-operator/pkg/apis/appsody/v1alpha1"
 	framework "github.com/operator-framework/operator-sdk/pkg/test"
-	appsody "github.com/appsody/appsody-operator/pkg/apis/appsody/v1alpha1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+var (
+	retryInterval        = time.Second * 5
+	timeout              = time.Minute * 3
+	cleanupRetryInterval = time.Second * 1
+	cleanupTimeout       = time.Second * 5
 )
 
 func TestAppsodyApplication(t *testing.T) {
 	appsodyApplicationList := &appsody.AppsodyApplicationList{
-		TypeMeta: metav1.TypeMeta {
-			Kind: "AppsodyApplication",
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "AppsodyApplication",
 			APIVersion: "appsody/v1alpha1",
 		},
 	}
 	err := framework.AddToFrameworkScheme(apis.AddToScheme, appsodyApplicationList)
 	if err != nil {
-		t.Fatlf("Failed to add CR scheme to framework: %v", err)
+		t.Fatalf("Failed to add CR scheme to framework: %v", err)
 	}
 
 	// t.Run("SimpleTest", )
 }
 
-
 // --- Test Functions ----
 
 func AppsodyBasicTest(t *testing.T, applicationTag string) {
+	ctx := framework.NewTestCtx(t)
+	defer ctx.Cleanup()
+	err := ctx.InitializeClusterResources(&framework.CleanupOptions{
+		TestContext:   ctx,
+		Timeout:       cleanupTimeout,
+		RetryInterval: retryInterval,
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Log("Cluster Resource Initialized")
+
+	namespace, err := ctx.GetNamespace()
+
+	if err != nil {
+		t.Fatal(err)
+	}
 }
