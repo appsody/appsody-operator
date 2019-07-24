e1934d4c45cb3013ce99858fc4dea69c2bf7e494
Justeen-Randev <Justeen.Randev@ibm.com>
2019-07-24 11:40:03 -0400

    Made basic test for scaling

--- test/e2e/appsody_test.go
+++ test/e2e/appsody_test.go
@@ -1,11 +1,13 @@
 package e2e
 
 import (
+	goctx "context"
+	"fmt"
 	"testing"
 	"time"
 
 	"github.com/appsody-operator/pkg/apis"
-	appsody "github.com/appsody-operator/pkg/apis/appsody/v1alpha1"
+	appsodyv1alpha1 "github.com/appsody-operator/pkg/apis/appsody/v1alpha1"
 	framework "github.com/operator-framework/operator-sdk/pkg/test"
 	"github.com/operator-framework/operator-sdk/pkg/test/e2eutil"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -19,7 +21,7 @@ var (
 )
 
 func TestAppsodyApplication(t *testing.T) {
-	appsodyApplicationList := &appsody.AppsodyApplicationList{
+	appsodyApplicationList := &appsodyv1alpha1.AppsodyApplicationList{
 		TypeMeta: metav1.TypeMeta{
 			Kind: "AppsodyApplication",
 		},
@@ -62,4 +64,40 @@ func appsodyBasicTest(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
+
+	if err = appsodyBasicScaleTest(t, f, ctx); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func appsodyBasicScaleTest(t *testing.T, f *framework.Framework, ctx *framework.TestCtx) error {
+	namespace, err := ctx.GetNamespace()
+	if err != nil {
+		return fmt.Errorf("could not get namespace: %v", err)
+	}
+
+	helper := int32(3)
+	exampleAppsody := &appsodyv1alpha1.AppsodyApplication{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "AppsodyApplication",
+			APIVersion: "appsody.example.com/v1alpha1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "example-appsody",
+			Namespace: namespace,
+		},
+		Spec: appsodyv1alpha1.AppsodyApplicationSpec{
+			ApplicationImage: "appsody:v1",
+			Replicas:         &helper,
+			Service: appsodyv1alpha1.AppsodyApplicationService{
+				Port: 8000,
+			},
+		},
+	}
+	err = f.Client.Create(goctx.TODO(), exampleAppsody, &framework.CleanupOptions{TestContext: ctx, Timeout: time.Second * 5, RetryInterval: time.Second * 1})
+	if err != nil {
+		return err
+	}
+
+	return err
 }
