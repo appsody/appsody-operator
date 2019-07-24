dc36543d62149010e5c9a89157d77bd8c00ee958
Justeen-Randev <Justeen.Randev@ibm.com>
2019-07-24 13:27:48 -0400

    Created second test for scaling

--- test/e2e/appsody_test.go
+++ test/e2e/appsody_test.go
@@ -11,6 +11,7 @@ import (
 	framework "github.com/operator-framework/operator-sdk/pkg/test"
 	"github.com/operator-framework/operator-sdk/pkg/test/e2eutil"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 )
 
 var (
@@ -70,14 +71,8 @@ func appsodyBasicTest(t *testing.T) {
 	}
 }
 
-func appsodyBasicScaleTest(t *testing.T, f *framework.Framework, ctx *framework.TestCtx) error {
-	namespace, err := ctx.GetNamespace()
-	if err != nil {
-		return fmt.Errorf("could not get namespace: %v", err)
-	}
-
-	helper := int32(3)
-	exampleAppsody := &appsodyv1alpha1.AppsodyApplication{
+func makeBasicAppsodyApplication(t *testing.T, f *framework.Framework, namespace string, helper int32) *appsodyv1alpha1.AppsodyApplication {
+	return &appsodyv1alpha1.AppsodyApplication{
 		TypeMeta: metav1.TypeMeta{
 			Kind:       "AppsodyApplication",
 			APIVersion: "appsody.example.com/v1alpha1",
@@ -94,10 +89,52 @@ func appsodyBasicScaleTest(t *testing.T, f *framework.Framework, ctx *framework.
 			},
 		},
 	}
+}
+
+func appsodyBasicScaleTest(t *testing.T, f *framework.Framework, ctx *framework.TestCtx) error {
+	namespace, err := ctx.GetNamespace()
+	if err != nil {
+		return fmt.Errorf("could not get namespace: %v", err)
+	}
+
+	helper := int32(3)
+
+	exampleAppsody := makeBasicAppsodyApplication(t, f, namespace, helper)
+
 	err = f.Client.Create(goctx.TODO(), exampleAppsody, &framework.CleanupOptions{TestContext: ctx, Timeout: time.Second * 5, RetryInterval: time.Second * 1})
 	if err != nil {
 		return err
 	}
 
+	err = e2eutil.WaitForOperatorDeployment(t, f.KubeClient, namespace, "example-appsody", 3, time.Second*5, time.Second*30)
+	if err != nil {
+		return err
+	}
+
+	if err = appsodyUpdateScaleTest(t, f, namespace, exampleAppsody); err != nil {
+		t.Fatal(err)
+	}
+
+	return err
+}
+
+func appsodyUpdateScaleTest(t *testing.T, f *framework.Framework, namespace string, exampleAppsody *appsodyv1alpha1.AppsodyApplication) error {
+	err := f.Client.Get(goctx.TODO(), types.NamespacedName{Name: "example-appsody", Namespace: namespace}, exampleAppsody)
+	if err != nil {
+		return err
+	}
+
+	helper2 := int32(4)
+	exampleAppsody.Spec.Replicas = &helper2
+	err = f.Client.Update(goctx.TODO(), exampleAppsody)
+	if err != nil {
+		return err
+	}
+
+	// wait for example-memcached to reach 4 replicas
+	err = e2eutil.WaitForOperatorDeployment(t, f.KubeClient, namespace, "example-appsody", 4, time.Second*5, time.Second*30)
+	if err != nil {
+		return err
+	}
 	return err
 }
