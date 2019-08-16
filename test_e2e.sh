94822546af36fb7260b337f66e1e846aeca9fa9e
Rui-Qi-Wang <Rui.Qi.Wang@ibm.com>
2019-08-16 15:12:42 -0400

    added missing command in script

--- test_e2e.sh
+++ test_e2e.sh
@@ -4,4 +4,4 @@ oc login -u system:admin
 oc create -f deploy/stack_defaults.yaml
 oc create -f deploy/crds/appsody_v1alpha1_appsodyapplication_crd.yaml
 oc create -f deploy/crds/appsody_v1alpha1_appsodyapplication_cr.yaml
-operator-sdk test ./test/e2e --namespace myproject
+operator-sdk test local ./test/e2e --namespace myproject
