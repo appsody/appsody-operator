b198a05fdae601f3a92b70bb1aeddf78b252d6fb
Rui-Qi-Wang <Rui.Qi.Wang@ibm.com>
2019-08-16 14:49:18 -0400

    put script in a folder

--- test_e2e.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-oc cluster up --skip-registry-check=true
-oc login -u system:admin
-oc create -f deploy/stack_defaults.yaml
-oc create -f deploy/crds/appsody_v1alpha1_appsodyapplication_crd.yaml
-oc create -f deploy/crds/appsody_v1alpha1_appsodyapplication_cr.yaml
-operator-sdk test ./test/e2e --up-local --namespace myproject
