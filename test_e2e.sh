1f98ca0fbe2d8244d14b4d2ce5d81e7e05402b1b
Rui-Qi-Wang <Rui.Qi.Wang@ibm.com>
2019-08-20 14:49:20 -0400

    removed namespace

--- test_e2e.sh
+++ test_e2e.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 oc cluster up --skip-registry-check=true
 oc login -u system:admin
-oc apply -f deploy/cluster_role_binding.yaml
-oc apply -f deploy/cluster_role.yaml
-oc apply -f deploy/service_account.yaml
-oc apply -f deploy/crds/appsody_v1alpha1_appsodyapplication_crd.yaml
-oc apply -f deploy/crds/appsody_v1alpha1_appsodyapplication_cr.yaml
+# oc apply -f deploy/cluster_role_binding.yaml
+# oc apply -f deploy/cluster_role.yaml
+# oc apply -f deploy/service_account.yaml
+# oc apply -f deploy/crds/appsody_v1alpha1_appsodyapplication_crd.yaml
+# oc apply -f deploy/crds/appsody_v1alpha1_appsodyapplication_cr.yaml
