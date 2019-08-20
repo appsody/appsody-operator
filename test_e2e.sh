de591070b6aace31f617e592808b7066d46761e2
Rui-Qi-Wang <Rui.Qi.Wang@ibm.com>
2019-08-20 14:13:05 -0400

    change instruction

--- test_e2e.sh
+++ test_e2e.sh
@@ -6,4 +6,3 @@ oc apply -f deploy/cluster_role.yaml
 oc apply -f deploy/service_account.yaml
 oc apply -f deploy/crds/appsody_v1alpha1_appsodyapplication_crd.yaml
 oc apply -f deploy/crds/appsody_v1alpha1_appsodyapplication_cr.yaml
-operator-sdk test local github.com/appsody-operator/test/e2e --namespace myproject
