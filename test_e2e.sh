0c9468e9a8d131b867b9b9646d26024dec78b657
Rui-Qi-Wang <Rui.Qi.Wang@ibm.com>
2019-08-20 13:50:48 -0400

    update travis

--- test_e2e.sh
+++ test_e2e.sh
@@ -6,5 +6,4 @@ oc apply -f deploy/cluster_role.yaml
 oc apply -f deploy/service_account.yaml
 oc apply -f deploy/crds/appsody_v1alpha1_appsodyapplication_crd.yaml
 oc apply -f deploy/crds/appsody_v1alpha1_appsodyapplication_cr.yaml
-operator-sdk up local
-operator-sdk test local ./test/e2e --up-local --namespace myproject
+operator-sdk test local github.com/appsody-operator/test/e2e --namespace myproject
