a23006a30ccd0b19e887865ca963e6fbb2c124f7
Justeen-Randev <Justeen.Randev@ibm.com>
2019-08-26 09:40:57 -0400

    Fixing the package name

--- test/e2e/appsody_test.go
+++ test/e2e/appsody_test.go
@@ -23,4 +23,10 @@ func TestAppsodyApplication(t *testing.T) {
 	t.Run("AppsodyPullPolicyTest", AppsodyPullPolicyTest)
 	t.Run("AppsodyBasicTest", AppsodyBasicTest)
 	t.Run("AppsodyStorageTest", AppsodyBasicStorageTest)
+	t.Run("AppsodyPullSecretTest", AppsodyPullSecretTest)
+	t.Run("AppsodyResourcesTest", AppsodyResourcesTest)
+	t.Run("AppsodyServiceAccountTest", AppsodyServiceAccountTest)
+	t.Run("AppsodyServicesTest", AppsodyServicesTest)
+	t.Run("AppsodyVolumesTest", AppsodyVolumesTest)
+
 }
