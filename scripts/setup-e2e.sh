cd89b2433f38d3315a159f8b217e568573b03fbb
Ethan Johnston <ethan.johnston@ibm.com>
2019-09-20 10:14:42 -0400

    Add insecure registry to docker daemon file.

--- scripts/setup-e2e.sh
+++ scripts/setup-e2e.sh
@@ -3,14 +3,7 @@
 set -o errexit
 set -o nounset
 
-# oc adm ca create-server-cert \
-#  --signer-cert=/etc/origin/master/ca.crt \
-#  --signer-key=/etc/origin/master/ca.key  \
-#  --signer-serial=/etc/origin/master/ca.serial.txt  \
-#  --hostnames=$(oc registry info) \
-#  --cert=/etc/secrets/registry.crt  \
-#  --key=/etc/secrets/registry.key
-
+echo " {\"insecure-registries\": [\"172.30.0.0/16\"] } " >> /etc/docker/daemon.json
 
 oc create sa robot
 oc policy add-role-to-user registry-viewer system:serviceaccounts
