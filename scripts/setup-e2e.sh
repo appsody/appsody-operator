af375d621540ae42e7d2f2aa1ffd5d789503bd08
Ethan Johnston <ethan.johnston@ibm.com>
2019-09-20 14:38:13 -0400

    Switch to developer user before attempting to use the docker daemon.

--- scripts/setup-e2e.sh
+++ scripts/setup-e2e.sh
@@ -3,9 +3,10 @@
 set -o errexit
 set -o nounset
 
-echo " {\"insecure-registries\": [\"172.30.0.0/16\"] } " >> /etc/docker/daemon.json
+
 
 oc create sa robot
+oc login -u developer
 oc policy add-role-to-user registry-viewer system:serviceaccounts
 oc policy add-role-to-user registry-editor system:serviceaccounts
 docker login -u robot -p $(oc sa get-token robot) $(oc registry info)
