d501fa4be8b42a90067a5ba20222389dafc67a4c
Ethan Johnston <ethan.johnston@ibm.com>
2019-09-23 12:11:00 -0400

    Use docker registry route instead of the registry IP

--- scripts/setup-e2e.sh
+++ scripts/setup-e2e.sh
@@ -12,5 +12,6 @@ oc create sa robot
 oc login -u developer
 oc policy add-role-to-user registry-viewer system:serviceaccounts
 oc policy add-role-to-user registry-editor system:serviceaccounts
-docker login -u robot -p $(oc sa get-token robot) $(oc registry info)
+oc get routes
+docker login -u robot -p $(oc sa get-token robot) $(oc get route docker-registry -o=jsonpath='{.spec.host}')
 
