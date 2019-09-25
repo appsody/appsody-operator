755345cfc1d3245b1c070273df282ca263e04b40
Ethan Johnston <ethan.johnston@ibm.com>
2019-09-25 10:37:02 -0400

    moving docker daemon configuration outside script to debug

--- scripts/setup-e2e.sh
+++ scripts/setup-e2e.sh
@@ -3,10 +3,6 @@
 set -o errexit
 set -o nounset
 
-# sudo - E env "PATH=$PATH" echo "{ \"insecure-registries\":[\"172.30.0.0/16\"] }" >> /etc/docker/daemon.json
-echo "{ \"insecure-registries\":[\"172.30.0.0/16\"] }" | sudo tee -a /etc/docker/daemon.json
-sudo service docker restart
-
 oc create sa robot
 oc login -u developer
 oc policy add-role-to-user registry-viewer system:serviceaccounts
