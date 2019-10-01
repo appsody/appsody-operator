cb386d8866f0a0f4909adc228587d52b7050024a
JusteenR <37389654+JusteenR@users.noreply.github.com>
2019-10-01 13:52:37 -0400

    E2e adding ConfigMaps tests (#113)
    * Added basic e2e testing skeleton
    
    * Extended basic e2e test skeleton
    
    * Created sample basic e2e deployment test.
    
    * Basic test now works with up-local.
    
    * Made basic test for scaling
    
    * Created second test for scaling
    
    * Refactored test directory to have utilities in own package.
    
    * Added attempt at storage persistence testing.
    
    * Updated WaitForDeployments to test deployments properly.
    
    * Completed function to check for stateful sets when persistence is enabled.
    
    * Updated e2e util waitForStatefulset log output for clarity
    
    * Test to check pull policy
    
    * Updates to fix logging errors on minishift
    
    * Removed temporary multipliers on timeouts
    
    * Updated default microprofile image and largely increased timeout constant.
    
    * Updated tests to run in their own contexts.
    
    * Refactored e2e tests into subdirectory
    
    * Added comments to the refactored tests
    
    * update stack defaults readiness probe to match master
    
    * Implemented basic e2e tests, squashing commits
    
    * Moved all e2e files into the same package to avoid warnings in operator-sdk logs
    
    * updated branch to include the deletion of /e2e/tests
    
    * New tests
    
    * Fixing the package name
    
    * Updated the import package for appsody
    
    * Autoscaling Tests
    
    * Revert "Autoscaling Tests"
    
    This reverts commit 15610e48ed020a8e14473703fc192be3b28b5861.
    
    Seperating Tests
    
    * Testing the autoscaling feature
    
    * Seperating tests
    
    * Fixed error messages
    
    * Checking Travis Build
    
    * Restructing for travis build
    
    * Changed amount of replicas to use less resources
    
    * Using new image to make travis build faster
    
    * Decreased resources used
    
    * Configmaps test
    
    * Test for the default configmap
    
    * ConfigMaps default and constants tests
    
    * Changed name of tests
    
    * Changed stack name for configmaps test as to not interfere with other tests
    
    * Removed unnecessary calls to get function and included fails when there are errors

---
+++ test/e2e/appsody_configmapsconst.go
@@ -0,0 +1,157 @@
+package e2e
+
+import (
+	goctx "context"
+	"testing"
+	"time"
+
+	"k8s.io/apimachinery/pkg/util/intstr"
+
+	appsodyv1beta1 "github.com/appsody/appsody-operator/pkg/apis/appsody/v1beta1"
+	"github.com/appsody/appsody-operator/test/util"
+	framework "github.com/operator-framework/operator-sdk/pkg/test"
+	e2eutil "github.com/operator-framework/operator-sdk/pkg/test/e2eutil"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+)
+
+// AppsodyConfigMapsConstTest : More indepth testing of configmap
+func AppsodyConfigMapsConstTest(t *testing.T) {
+
+	ctx, err := util.InitializeContext(t, cleanupTimeout, retryInterval)
+	defer ctx.Cleanup()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	namespace, err := ctx.GetNamespace()
+	if err != nil {
+		t.Fatalf("Couldn't get namespace: %v", err)
+	}
+
+	t.Logf("Namespace: %s", namespace)
+
+	f := framework.Global
+
+	// Values to be loaded in the constants configmap
+	updateData := map[string]string{"jstack": `{"version": 1.0.0,"expose":true, "service":{"port": 3000,"type": NodePort}, "livenessProbe":{"failureThreshold": 8, "httpGet":{"path": /live, "port": 3000}, "initialDelaySeconds": 8, "periodSeconds": 2}, "readinessProbe":{"failureThreshold": 12, "httpGet":{"path": /ready, "port": 3000}, "initialDelaySeconds": 5, "periodSeconds": 2, "timeoutSeconds": 1}}`}
+	configMap := &corev1.ConfigMap{}
+
+	// Gets the configmap that contains the constant values that will be applied to the appsody application and cannot be changed
+	err = f.Client.Get(goctx.TODO(), types.NamespacedName{Name: "appsody-operator-constants", Namespace: namespace}, configMap)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Sets constant values
+	configMap.Data = updateData
+
+	err = f.Client.Update(goctx.TODO(), configMap)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Creating a basic appsody application that specifies new values for fields that are already assigned from the constants configmap
+	replicas := int32(1)
+	probe := corev1.Handler{
+		HTTPGet: &corev1.HTTPGetAction{
+			Path: "/health",
+			Port: intstr.FromInt(3000),
+		},
+	}
+	expose := false
+	serviceType := corev1.ServiceTypeClusterIP
+	apps := &appsodyv1beta1.AppsodyApplication{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "AppsodyApplication",
+			APIVersion: "appsody.dev/v1beta1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "example-appsody-constconfigmaps",
+			Namespace: namespace,
+		},
+		Spec: appsodyv1beta1.AppsodyApplicationSpec{
+			ApplicationImage: "navidsh/demo-day",
+			Replicas:         &replicas,
+			Stack:            "jstack",
+			Expose:           &expose,
+			Service: &appsodyv1beta1.AppsodyApplicationService{
+				Type: &serviceType,
+			},
+			LivenessProbe: &corev1.Probe{
+				InitialDelaySeconds: 4,
+				PeriodSeconds:       5,
+				FailureThreshold:    6,
+				Handler:             probe,
+			},
+			ReadinessProbe: &corev1.Probe{
+				InitialDelaySeconds: 4,
+				PeriodSeconds:       5,
+				FailureThreshold:    6,
+				TimeoutSeconds:      2,
+				Handler:             probe,
+			},
+		},
+	}
+
+	err = f.Client.Create(goctx.TODO(), apps, &framework.CleanupOptions{TestContext: ctx, Timeout: time.Second, RetryInterval: time.Second})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// wait for example-appsody-constconfigmaps to reach 1 replicas
+	err = e2eutil.WaitForDeployment(t, f.KubeClient, namespace, "example-appsody-constconfigmaps", 1, retryInterval, timeout)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// creates a new struct for the appsody application otherwise the probe values get overwritten
+	apps = &appsodyv1beta1.AppsodyApplication{}
+	err = f.Client.Get(goctx.TODO(), types.NamespacedName{Name: "example-appsody-constconfigmaps", Namespace: namespace}, apps)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// update the application to two replicas
+	helper := int32(2)
+	apps.Spec.Replicas = &helper
+
+	err = f.Client.Update(goctx.TODO(), apps)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = e2eutil.WaitForDeployment(t, f.KubeClient, namespace, "example-appsody-constconfigmaps", 2, retryInterval, timeout)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// checks none of the values that were specified in the constants configmap are changed
+	if *apps.Spec.Expose == true {
+		t.Log("Expose in configmap constants is applied and not changed")
+	} else {
+		t.Fatal("Expose in configmap constants is not applied")
+	}
+
+	serviceType = corev1.ServiceTypeNodePort
+	if apps.Spec.Service.Port == 3000 && *apps.Spec.Service.Type == serviceType {
+		t.Log("Service from configmap constants is applied and not changed")
+	} else {
+		t.Fatal("Service in configmap constants is not applied")
+	}
+
+	port := intstr.IntOrString{IntVal: 3000}
+	if apps.Spec.ReadinessProbe.FailureThreshold == 12 && apps.Spec.ReadinessProbe.HTTPGet.Path == "/ready" && apps.Spec.ReadinessProbe.HTTPGet.Port == port && apps.Spec.ReadinessProbe.InitialDelaySeconds == 5 && apps.Spec.ReadinessProbe.PeriodSeconds == 2 && apps.Spec.ReadinessProbe.TimeoutSeconds == 1 {
+		t.Log("ReadinessProbe in configmap constants is applied and not changed")
+	} else {
+		t.Fatal("ReadinessProbe in configmap constants is not applied")
+	}
+
+	if apps.Spec.LivenessProbe.FailureThreshold == 8 && apps.Spec.LivenessProbe.HTTPGet.Path == "/live" && apps.Spec.LivenessProbe.HTTPGet.Port == port && apps.Spec.LivenessProbe.InitialDelaySeconds == 8 && apps.Spec.LivenessProbe.PeriodSeconds == 2 {
+		t.Log("LivenessProbe in configmap constants is applied and not changed")
+	} else {
+		t.Fatal("LivenessProbe in configmap constants is not applied")
+	}
+
+}
