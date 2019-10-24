e6605d0067c1cbe00f2995be1c4e86a88d1277d1
Ethan Johnston <ethan.johnston@ibm.com>
2019-10-24 16:54:17 -0400

    Verify that knative is installed before running knative test

--- test/e2e/appsody_knative.go
+++ test/e2e/appsody_knative.go
@@ -8,16 +8,18 @@ import (
 	"github.com/appsody/appsody-operator/test/util"
 	framework "github.com/operator-framework/operator-sdk/pkg/test"
 	e2eutil "github.com/operator-framework/operator-sdk/pkg/test/e2eutil"
+	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	dynclient "sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 // AppsodyKNativeTest verify functionality of kNative option in appsody
-func AppsodyKNativeTest(t *testing.T) {
+func AppsodyKnativeTest(t *testing.T) {
 	ctx, err := util.InitializeContext(t, cleanupTimeout, retryInterval)
 	if err != nil {
 		t.Fatal(err)
 	}
 	defer ctx.Cleanup()
-
 	namespace, err := ctx.GetNamespace()
 	if err != nil {
 		t.Fatalf("Couldn't get namespace: %v", err)
@@ -27,24 +29,43 @@ func AppsodyKNativeTest(t *testing.T) {
 
 	f := framework.Global
 
-	err = e2eutil.WaitForOperatorDeployment(t, f.KubeClient, namespace, "appsody-operator", 1, retryInterval, operatorTimeout)
-	if err != nil {
-		util.FailureCleanup(t, f, namespace, err)
-	}
-	knativeBool := true
-	applicationName := "example-appsody-knative"
+	if isKnativeInstalled(t, f) {
+		err = e2eutil.WaitForOperatorDeployment(t, f.KubeClient, namespace, "appsody-operator", 1, retryInterval, operatorTimeout)
+		if err != nil {
+			util.FailureCleanup(t, f, namespace, err)
+		}
+		knativeBool := true
+		applicationName := "example-appsody-knative"
 
-	exampleAppsody := util.MakeBasicAppsodyApplication(t, f, applicationName, namespace, 1)
-	exampleAppsody.Spec.CreateKnativeService = &knativeBool
+		exampleAppsody := util.MakeBasicAppsodyApplication(t, f, applicationName, namespace, 1)
+		exampleAppsody.Spec.CreateKnativeService = &knativeBool
 
-	// Create application deployment and wait
-	err = f.Client.Create(goctx.TODO(), exampleAppsody, &framework.CleanupOptions{TestContext: ctx, Timeout: time.Second, RetryInterval: time.Second})
-	if err != nil {
-		util.FailureCleanup(t, f, namespace, err)
-	}
+		// Create application deployment and wait
+		err = f.Client.Create(goctx.TODO(), exampleAppsody, &framework.CleanupOptions{TestContext: ctx, Timeout: time.Second, RetryInterval: time.Second})
+		if err != nil {
+			util.FailureCleanup(t, f, namespace, err)
+		}
 
-	err = util.WaitForKnativeDeployment(t, f, namespace, applicationName, retryInterval, timeout)
-	if err != nil {
-		util.FailureCleanup(t, f, namespace, err)
+		err = util.WaitForKnativeDeployment(t, f, namespace, applicationName, retryInterval, timeout)
+		if err != nil {
+			util.FailureCleanup(t, f, namespace, err)
+		}
+	} else {
+		t.Log("Knative is not installed on this cluster, skipping AppsodyKnativeTest...")
 	}
+}
+
+func isKnativeInstalled(t *testing.T, f *framework.Framework) bool {
+	deployments := &corev1.PodList{}
+	options := &dynclient.ListOptions{
+		Namespace: "knative-serving",
+	}
+	err := f.Client.List(goctx.TODO(), options, deployments)
+	if err != nil {
+		if apierrors.IsNotFound(err) {
+			return false
+		}
+		t.Fatalf("Error occurred while trying to find knative-serving %v", err)
+	}
+	return true
 }
