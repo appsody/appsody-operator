e103284b5ff7299d8f871bf4fb9ae1a7bc57ac2f
Ethan Johnston <ethan.johnston@ibm.com>
2020-03-18 10:32:05 -0400

    Fix OpenShift annotation for version control uri

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -6,12 +6,13 @@ import (
 	oputils "github.com/application-stacks/runtime-component-operator/pkg/utils"
 )
 
+// GetOpenShiftAnnotations add any additional annotations that are provided by the CLI tool
 func GetOpenShiftAnnotations(ba common.BaseComponent) map[string]string {
 	annos := map[string]string{}
 
 	for key, val := range ba.GetLabels() {
 		if key == "image.opencontainers.org/source" {
-			annos["app.openshift.io/vcs-ref"] = val
+			annos["app.openshift.io/vcs-uri"] = val
 		}
 	}
 
